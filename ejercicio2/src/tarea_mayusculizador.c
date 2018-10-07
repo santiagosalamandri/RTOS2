f2c502e935b6b2967631c336ad18711cff518924
JinnKaY <menendez91@live.com.ar>
2018-10-07 13:24:38 -0300

    Agregados SRC

---
+++ ejercicio2/src/tarea_mayusculizador.c
@@ -0,0 +1,36 @@
+#include "tarea_mayusculizador.h"
+
+static void mayusculizar(mensaje_entre_tareas_t* mensajeEntreTareas);							// Funcion mayusculizadora
+extern void stackReport(void);																	// Reporte de stack disponible
+extern void sizeReport(void);
+
+extern header_t headerEnProceso;
+
+// Implementacion de funcion de la tarea
+void mayusculizador_task(void* taskParmPtr)
+{
+	mensaje_entre_tareas_t mensajeEntreTareas;
+
+	while (TRUE) // ---------- REPETIR POR SIEMPRE --------------------------
+	{
+		xQueueReceive(queMayusculizar, &mensajeEntreTareas, portMAX_DELAY);						// Desencolar dato
+		headerEnProceso.tiempo_de_fin =  xTaskGetTickCount();
+		mayusculizar(&mensajeEntreTareas);														// Mayusculizar dato
+		xQueueSend(queTransmision, &mensajeEntreTareas, portMAX_DELAY);							// Encolar resultado
+		stackReport();																			// Reportar stack disponible
+		sizeReport();
+	}
+}
+
+static void mayusculizar(mensaje_entre_tareas_t* mensajeEntreTareas)
+{
+	for (uint32_t dataIndex = HEADER_LENGTH; dataIndex < (mensajeEntreTareas->length + HEADER_LENGTH); dataIndex++)
+	{	// Iterar entre la porcion de paquete correspondiente al payload
+
+		if (mensajeEntreTareas->buffer[dataIndex] >= LETRA_a && mensajeEntreTareas->buffer[dataIndex] <= LETRA_z)
+		{	// Si se encuentra entre 'a' y 'z' entonces pasar a mayuscula restando la diferencia entre mayus y minus.
+			mensajeEntreTareas->buffer[dataIndex] = mensajeEntreTareas->buffer[dataIndex] - DIFERENCIA_ASCII_MINUSCULA_MAYUSCULA;
+		}
+	}
+}
+
